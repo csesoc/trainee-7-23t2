edc6dfdd743b7505ae14b9366f8ee3645bf76df8
Rolguard <76728853+Rolguard@users.noreply.github.com>
2023-07-23 15:48:10 +1000

    Adding components folder with some pages

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+// import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>My landing page</h1>
+    </div>
+  );
+};
+export default LandingPage;
---
+++ client/src/components/MainPage/MainPage.js
@@ -0,0 +1,8 @@
+const MainPage = () => {
+  return (
+    <div>
+      <h1>This is the main page!!</h1>
+    </div>
+  );
+};
+export default MainPage;
