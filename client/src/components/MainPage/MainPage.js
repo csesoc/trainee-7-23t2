8b32e38caf3488f0378b0a99824072a2fc0d0b32
eningfu <122142631+eningfu@users.noreply.github.com>
2023-09-02 13:08:04 +1000

    sidebar tests and basic css

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,7 +1,42 @@
+import "./MainPage.css";
+
 const MainPage = () => {
   return (
-    <div>
-      <h1>This is the main page!!</h1>
+    <div class="wrapper">
+      <div class="sidebar">
+        <div class="iconBar">
+          <ul class="icons">
+            <a href="#Friends">👥</a>
+            <a href="#findFriends">🔍</a>
+            <a href="#inbox">📩</a>
+            <a href="#settingsPage">🔧</a>
+          </ul>
+        </div>
+        <div class="pageType">
+          <h3>Add Friends</h3>
+        </div>
+        <div class="search">
+          <form action="#">
+            <input type="text"
+              placeholder=" Search..."
+              name="search"/>
+            <button>
+              🔍
+            </button>
+          </form>
+        </div>
+        <ul class="listPeople">
+          <li class="person">
+            {/* temporary */}
+            <span class="dot"></span>
+            <h3>Name</h3>
+            <p>Status</p>
+          </li>
+        </ul>
+      </div>
+      <div class="map">
+        <p>this is the map</p>
+      </div>
     </div>
   );
 };
